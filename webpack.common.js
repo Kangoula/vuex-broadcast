89c837ae7fc3b0efb1fc0358dad995d47500b7ca
gdenis <guillaume.denis@two-i.fr>
2020-04-06 12:10:55 +0200

    update build

--- webpack.common.js
+++ webpack.common.js
@@ -4,6 +4,22 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
   entry: './src/index.js',
   plugins: [new CleanWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      }
+    ]
+  },
+
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'vuex-broadcast.js',
